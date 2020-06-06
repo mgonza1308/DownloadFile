5d3d7263eae46a70ebf7b3689e84648234a8cdc7
Mauricio Gonzalez <impgonzalez123@gmail.com>
2020-06-05 22:09:41 -0700

    muni

--- src/test/java/ChkFil/Download.java
+++ src/test/java/ChkFil/Download.java
@@ -1,51 +1,78 @@
 package ChkFil;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-
-import static org.testng.Assert.assertTrue;
-
-import java.io.File;
-import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
 
 public class Download {
 
-	private WebDriver driver;
-	private String downloadFilePath = "C:\\Users\\casa\\Downloads";
-  @BeforeClass
-  public void beforeClass() {
-	  System.setProperty("webdriver.chrome.driver", "./src/test/resources/Driver/chromedriver.exe");
-	  HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-	  chromePrefs.put("profile.default_content_setting.popups", 0);
-	  chromePrefs.put("download.default_directory", downloadFilePath);
-	  
-	  //Here setting options Chrome, Change open windows to download file directly
-	  ChromeOptions options = new ChromeOptions();
-	  options.setExperimentalOption("prefs", chromePrefs);
-	  driver = new ChromeDriver(options);
-	  	  
-  }
+//	@FindBy(xpath="//a[contains(@href,'municipalidad')]")
+//	public List<WebElement> MuniLink;
+	
+	@FindBy(xpath="//*[@id=\"content\"]")
+	public List<WebElement> MuniRegion;
+	
   @Test
   public void downloaFile() throws InterruptedException {
-	  driver.get("http://the-internet.herokuapp.com/download");
-	  driver.findElement(By.cssSelector("div.example>a")).click();
-	  Thread.sleep(2000);
-	  
-	  //Find all files in folder
-	  File folder = new File(downloadFilePath);
-	  File[] lisofFile = folder.listFiles();
-	  assertTrue(lisofFile.length>0, "Dont file in folder");
+	  try {
+		  System.setProperty("webdriver.chrome.driver", "./src/test/resources/Driver/chromedriver.exe"); 
+		  WebDriver driver = new ChromeDriver();
+		  driver.get("https://www.la-municipalidad.cl/");
+		  JavascriptExecutor js = (JavascriptExecutor) driver;
+		  WebElement Element = driver.findElement(By.xpath("//h2[contains(text(),'Región del país')]"));
+		  js.executeScript("arguments[0].scrollIntoView();", Element); 
+		  List<WebElement> region = driver.findElements(By.xpath("//a[contains(@href,'region')]"));
+		  for (int i=1; i<=region.size(); i++) {
+			
+			  String origen = region.get(i).getAttribute("href");
+			  System.out.println("Url: "+origen);
+			  region.get(i).click();
+			  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			  List<WebElement> MuniLink = driver.findElements(By.xpath("//a[contains(@href,'municipalidad')]"));
+			  for (int t =1; t<=MuniLink.size(); t++) {
+
+				  
+				  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				  
+
+				 String muniComunidad = MuniLink.get(t).getAttribute("href");
+				 System.out.println("Munoi de local: "+muniComunidad);
+				 driver.get(muniComunidad);
+//				  MuniLink.get(t).click();
+				  
+				  WebElement dire = driver.findElement(By.xpath("//span[@class='street-address']"));
+				  System.out.println("Direccion"+dire.getText());
+				  
+				  driver.get(origen);
+				  String pages = driver.getWindowHandle();
+				  System.out.println("pagina: "+pages.toString());
+				  driver.switchTo().window(pages);
+
+
+			  }
+			  js.executeScript("window.history.go(-1)");
+		  }
+
+		  
+	  }catch (StaleElementReferenceException e) {
+		  System.out.println("Error: "+e);
+	  }
   }
 
 
   @AfterClass
   public void afterClass() {
+	  //driver.close();
   }
 
 }
---
+++ src/test/java/ChkFil/FileDelete.java
@@ -0,0 +1,6 @@
+package ChkFil;
+
+public class FileDelete {
+
+		
+}
